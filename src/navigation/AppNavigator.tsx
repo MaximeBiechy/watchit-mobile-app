6785c71500b83715bda0c7b3218e257b89274603
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-18 11:50:04 +0100

    feat: integrate react-native-bootsplash for enhanced splash screen experience

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import RNBootSplash from 'react-native-bootsplash';
 import HomeScreen from '../screens/index.ts';
 
 const Stack = createNativeStackNavigator();
 
 function AppNavigator() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    <NavigationContainer onReady={() => RNBootSplash.hide({ fade: true })}>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
