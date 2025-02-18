fefe922a8be41f050ead06c942a4789f2fa77ad2
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-18 18:06:50 +0100

    feat: update highlight color to a new shade

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,6 @@
 export const primaryColor = '#2D3E50';
 export const gray = '#5D6D7D';
-export const highlightColor = '#FFC107';
+export const highlightColor = '#4990E2';
 export const accentColor = '#FF8700';
 export const darkGray = '#3C4B63';
 export const errorColor = '#D42525';
