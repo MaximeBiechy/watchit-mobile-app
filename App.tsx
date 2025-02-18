555338e255a42c133e7743377e0378490ed263ef
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-18 18:09:09 +0100

    feat: disable require-default-props rule in ESLint configuration

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 import Toast from 'react-native-toast-message';
@@ -8,10 +8,12 @@ import AppNavigator from './src/navigation/AppNavigator.tsx';
 import i18n from './src/locales/i18n.ts';
 import store from './src/store/store.ts';
 import { toastConfig } from './src/utils/toast.tsx';
+import { primaryColor } from './src/styles/colors.ts';
 
 function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar barStyle="light-content" backgroundColor={primaryColor} />
       <SafeAreaView style={{ flex: 1 }}>
         <Provider store={store}>
           <I18nextProvider i18n={i18n}>
