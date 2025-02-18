71ef80899e1ef256cf6586f7ddff64c573c466bb
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-18 18:06:43 +0100

    feat: add social media images to assets configuration

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,10 +1,22 @@
 // @ts-nocheck / disable checking for TypeScript files
+// IMAGES
 import logo from './images/app-logo.png';
+// IMAGES/SOCIAL MEDIA
+import apple from './images/social-media/apple.png';
+import facebook from './images/social-media/facebook.png';
+import google from './images/social-media/google.png';
+
+// ICONS
 import eyeHidden from './icons/eye-hidden.svg';
 
 const assets = {
   images: {
     logo,
+    SocialsMedia: {
+      apple,
+      facebook,
+      google,
+    },
   },
   icons: {
     eyeHidden,
