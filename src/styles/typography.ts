2dbc15c1fef78002494d4ceb2be353cdfacf3700
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-18 15:45:15 +0100

    feat: update typography configuration with new font styles and exports

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,14 +1,33 @@
 import fonts from '../assets/fonts.ts';
 
-export const headingFont = fonts.Montserrat.semiBold;
-export const bodyFont = fonts.Montserrat.medium;
+export const headingTitleFont = fonts.Montserrat.semiBold;
+export const headingSectionFont = fonts.Poppins.semiBold;
 
-export const heading = {
-  fontFamily: headingFont,
-  fontSize: 24,
+export const bodyInfoFont = fonts.Montserrat.medium;
+export const bodyTextFont = fonts.Poppins.regular;
+export const bodyLongTextFont = fonts.Roboto.regular;
+
+export const navigationTopBarTitle = {
+  fontFamily: headingTitleFont,
+  fontSize: 16,
 };
 
-export const body = {
-  fontFamily: bodyFont,
-  fontSize: 16,
+export const sectionTitle = {
+  fontFamily: headingSectionFont,
+  fontSize: 18,
+};
+
+export const bodyInfo = {
+  fontFamily: bodyInfoFont,
+  fontSize: 12,
+};
+
+export const bodyText = {
+  fontFamily: bodyTextFont,
+  fontSize: 12,
+};
+
+export const bodyLongText = {
+  fontFamily: bodyLongTextFont,
+  fontSize: 14,
 };
