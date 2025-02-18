4cafd5258092c6b5fbfa02b11a6f10d0a75fdf69
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-18 15:00:58 +0100

    feat: add asset management images and logos

---
+++ src/assets/assets.ts
@@ -0,0 +1,14 @@
+// @ts-nocheck / disable checking for TypeScript files
+import logo from './images/app-logo.png';
+import eyeHidden from './icons/eye-hidden.svg';
+
+const assets = {
+  images: {
+    logo,
+  },
+  icons: {
+    eyeHidden,
+  },
+};
+
+export default assets;
