d7e17b6c2e1ac60f3bfb673bf15e5feb7aec175a
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-19 11:57:28 +0100

    feat: add login translations for English and French

--- src/locales/translations/index.ts
+++ src/locales/translations/index.ts
@@ -1,12 +1,16 @@
 import enChooseConnectionMode from './en/chooseConnectionMode.json';
 import frChooseConnectionMode from './fr/chooseConnectionMode.json';
+import enLogin from './en/login.json';
+import frLogin from './fr/login.json';
 
 const translations = {
   en: {
     chooseConnectionMode: enChooseConnectionMode,
+    login: enLogin,
   },
   fr: {
     chooseConnectionMode: frChooseConnectionMode,
+    login: frLogin,
   },
 };
 
