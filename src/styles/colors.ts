1fe8ad2d1ff1b3691d6b08806b376c0ac4adcefb
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-19 14:31:01 +0100

    feat: add subTitleColor for improved theming options

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -3,6 +3,7 @@ export const gray = '#5D6D7D';
 export const highlightColor = '#4990E2';
 export const accentColor = '#FF8700';
 export const darkGray = '#3C4B63';
+export const subTitleColor = '#EBEBEF';
 export const errorColor = '#D42525';
 export const warningColor = '#FF8C04';
 export const successColor = '#2A9D8F';
