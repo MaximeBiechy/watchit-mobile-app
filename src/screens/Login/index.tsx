6878f9966125fd4bb8e01d8b7e014fa0e1849286
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-19 14:29:40 +0100

    feat: implement Login screen with form handling and translations

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,9 +1,39 @@
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import FormComponent from '../../components/Form/FormComponent.tsx';
+import styles from './styles.ts';
 
 function LoginScreen() {
+  const { t } = useTranslation('login');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Handle login logic here
+  };
+
+  const inputs = [
+    {
+      placeholder: t('placeholderEmail'),
+      value: email,
+      onChangeText: setEmail,
+    },
+    {
+      placeholder: t('placeholderPassword'),
+      value: password,
+      onChangeText: setPassword,
+      secureTextEntry: true,
+    },
+  ];
+
   return (
     <View>
-      <Text>Login Screen</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>{t('title')}</Text>
+        <Text style={styles.subtitle}>{t('subtitle')}</Text>
+      </View>
+      <FormComponent inputs={inputs} onSubmit={handleLogin} submitButtonTitle={t('submitButtonTitle')} />
     </View>
   );
 }
---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+import { authTitle, bodyInfo } from '../../styles/typography.ts';
+import { subTitleColor } from '../../styles/colors.ts';
+
+const style = StyleSheet.create({
+  titleContainer: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    marginVertical: 60,
+    gap: 15,
+  },
+  title: {
+    ...authTitle,
+    color: 'white',
+  },
+  subtitle: {
+    ...bodyInfo,
+    color: subTitleColor,
+  },
+});
+
+export default style;
