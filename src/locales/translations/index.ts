29bb4cd73124538af5e05f4b071641673f295778
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-02-20 12:47:33 +0100

    feat: add email verification JSON files and rename existing JSON files

--- src/locales/translations/index.ts
+++ src/locales/translations/index.ts
@@ -1,20 +1,27 @@
-import enChooseConnectionMode from './en/chooseConnectionMode.json';
-import frChooseConnectionMode from './fr/chooseConnectionMode.json';
-import enLogin from './en/login.json';
-import frLogin from './fr/login.json';
-import enSignup from './en/signup.json';
-import frSignup from './fr/signup.json';
+// Auth
+import enChooseConnectionMode from './en/auth/chooseConnectionMode.json';
+import frChooseConnectionMode from './fr/auth/chooseConnectionMode.json';
+import enLogin from './en/auth/login.json';
+import frLogin from './fr/auth/login.json';
+import enSignup from './en/auth/signup.json';
+import frSignup from './fr/auth/signup.json';
+import enEmailVerification from './en/auth/emailVerification.json';
+import frEmailVerification from './fr/auth/emailVerification.json';
+// Onboarding
+// Main
 
 const translations = {
   en: {
     chooseConnectionMode: enChooseConnectionMode,
     login: enLogin,
     signup: enSignup,
+    emailVerification: enEmailVerification,
   },
   fr: {
     chooseConnectionMode: frChooseConnectionMode,
     login: frLogin,
     signup: frSignup,
+    emailVerification: frEmailVerification,
   },
 };
 
