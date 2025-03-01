f73bc7505bd4908ce55ae43f605f8bc7cb24fcf2
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-03-01 23:57:07 +0100

    feat: reorganize imports to include My List and User Profile screens in the main export

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,6 +1,3 @@
-// MainStack
-import HomeScreen from './Home/index.tsx';
-import DetailsScreen from './Details/index.tsx';
 // Auth Stack
 import ChooseConnectionModeScreen from './ChooseConnectionMode/index.tsx';
 import SignInScreen from './SignIn/index.tsx';
@@ -9,14 +6,23 @@ import EmailVerification from './EmailVerification/index.tsx';
 // Onboarding Stack
 import OnboardingScreen from './Onboarding/index.tsx';
 import ChooseAvatar from './ChooseAvatar/index.tsx';
+// Home Stack
+import HomeScreen from './Home/index.tsx';
+import DetailsScreen from './Details/index.tsx';
+// My List Stack
+import MyList from './MyList/index.tsx';
+// Profile Stack
+import UserProfile from './UserProfile/index.tsx';
 
 export {
-  HomeScreen,
-  DetailsScreen,
   ChooseConnectionModeScreen,
   SignInScreen,
   SignUpScreen,
   EmailVerification,
   OnboardingScreen,
   ChooseAvatar,
+  HomeScreen,
+  DetailsScreen,
+  MyList,
+  UserProfile,
 };
