60d9297c2b31c40a8c62307a1fba6cf3c5a31e98
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-03-03 23:45:14 +0100

    feat: enhance MyListNavigator and ProfileNavigator with responsive padding and animation options

--- src/navigation/ProfileNavigator.tsx
+++ src/navigation/ProfileNavigator.tsx
@@ -2,12 +2,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ProfileStackParamList } from './RootStackParamList.tsx';
 import { UserProfile } from '../screens/index.ts';
 import { renderHeader } from '../components/Header/HeaderComponent.tsx';
+import { PADDING_HORIZONTAL, PADDING_VERTICAL } from '../styles/responsives.ts';
+import { animation, animationDuration } from '../styles/transitionScreens.ts';
 
 const ProfileStack = createNativeStackNavigator<ProfileStackParamList>();
 
 function ProfileNavigator() {
   return (
-    <ProfileStack.Navigator initialRouteName="UserProfile">
+    <ProfileStack.Navigator
+      initialRouteName="UserProfile"
+      screenOptions={{
+        contentStyle: { paddingHorizontal: PADDING_HORIZONTAL, paddingTop: PADDING_VERTICAL },
+        animation, // ? Avoid flickering effect when navigating between screens
+        animationDuration, // ? It doesn't seem to work
+      }}
+    >
       <ProfileStack.Screen
         name="UserProfile"
         component={UserProfile}
