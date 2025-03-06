158362c4e54124c358511cd2f473d7d132bc9295
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-03-06 12:09:20 +0100

    feat: add styles for container with top margin in styles.ts

---
+++ src/screens/EditProfile/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { PADDING_VERTICAL } from '../../styles/responsives.ts';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: PADDING_VERTICAL * 4,
+  },
+});
+
+export default styles;
