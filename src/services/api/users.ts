9df9cf53cd47521a82474015c0f10afd11d7270f
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-03-17 23:01:52 +0100

    feat: enhance user data loading with API check and implement search text length validation

--- src/services/api/users.ts
+++ src/services/api/users.ts
@@ -1,5 +1,18 @@
 import api from './api.ts';
 
+export const getUserById = async (userId: string) => {
+  try {
+    const response = await api.get(`/users/${userId}`);
+
+    return response.data;
+  } catch (error: any) {
+    // const errorCode = error.response?.data?.code || 'UnexpectedError';
+    // const translatedMessage = t(`errors:${errorCode}`);
+    // showToast('error', translatedMessage);
+    return { error: error.response?.data?.message || 'An error occurred' };
+  }
+};
+
 export const updateUserSettings = async (userId: string, settings: any) => {
   try {
     await api.patch(`/users/${userId}/settings`, {
@@ -118,7 +131,6 @@ export const rateMedia = async (userId: string, mediaId: number, mediaType: stri
     return { error: error.response?.data?.message || 'An error occurred' };
   }
 };
-
 
 export const updateMediaRating = async (userId: string, mediaId: number, mediaType: string, rating: number) => {
   try {
